313376ddca2764a0e59899cac0533c1500bd8742
kulpa <sylwester.kulpa@nokia.com>
2023-03-21 14:30:44 +0100

    Simplify LoginRequired component

--- src/auth/components/LoginRequired/LoginRequired.tsx
+++ src/auth/components/LoginRequired/LoginRequired.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import ApiClient from "../../../utils/ApiClient";
 import useAuth from "../../hooks/useAuth";
 
 interface LoginRequiredProps {
@@ -14,31 +13,11 @@ const LoginRequired: React.FC<LoginRequiredProps> = (
 ) => {
   const auth = useAuth();
   const location = useLocation();
-  const apiClient = new ApiClient();
-  const [tokenVerified, setTokenVerified] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-
-  React.useEffect(() => {
-    if (auth.session && !loading) {
-      setLoading(true);
-      apiClient.post(
-        "/auth/verify-token/", {"token": auth.session.access}
-      ).then(
-        () => {
-          setTokenVerified(true);
-          setLoading(false);
-        }
-      ).catch(() => {
-        auth.logout();
-        setLoading(false);
-      });
-    }
-  }, [auth, apiClient, tokenVerified, loading])
 
   if (!auth.isAuthenticated()) {
     return <Navigate to="/auth/login/" state={{from: location}} replace />;
   }
-  return tokenVerified ? children : <></>
+  return children;
 }
 
 
